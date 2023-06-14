14b55d6bbd3c4473961e9a41eb84503b3c791cdb
aztsyahir <2021603394@student.uitm.edu.my>
2023-06-14 21:44:14 +0800

    add user register

--- src/main/java/com/heroku/java/customerController.java
+++ src/main/java/com/heroku/java/customerController.java
@@ -1,10 +1,10 @@
 package com.heroku.java;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,6 +14,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 // import java.util.ArrayList;
 // import java.util.Map;
+import java.sql.SQLException;
 
 // import org.jscience.physics.amount.Amount;
 // import org.jscience.physics.model.RelativisticModel;
@@ -29,24 +30,35 @@ public class customerController {
     }
 
     @PostMapping("/userregister")
-    public String AddCustomer(HttpSession session, @ModelAttribute("userregister") customer cust){
+    public String AddCustomer(HttpSession session, @ModelAttribute("userregister")customer cust){
 
         try{
             Connection connection = dataSource.getConnection();
             String sql = "INSERT INTO customer(custname, custemail, custphonenum, custaddress, custpassword) VALUES (?, ?, ?, ?, ?)";
             final var statement = connection.prepareStatement(sql);
 
-            statement.setString(1, customer.getcustname());
-            statement.setString(2, customer.getcustemail());
-            statement.setString(3, customer.getcustphonenum());
-            statement.setString(4, customer.getcustaddress());
-            statement.setString(5, customer.getcustpassword());
+            statement.setString(1, cust.getCustname());
+            statement.setString(2, cust.getCustemail());
+            statement.setString(3, cust.getCustphonenum());
+            statement.setString(4, cust.getCustaddress());
+            statement.setString(5, cust.getCustpassword());
 
             statement.executeUpdate();
 
             connection.close();
-            return "redirect/login";
-        }
+            return "redirect:/";
+        }catch (SQLException sqe) {
+      System.out.println("Error Code = " + sqe.getErrorCode());
+      System.out.println("SQL state = " + sqe.getSQLState());
+      System.out.println("Message = " + sqe.getMessage());
+      System.out.println("printTrace /n");
+      sqe.printStackTrace();
+
+      return "redirect:/userregister";
+    } catch (Exception e) {
+      System.out.println("E message : " + e.getMessage());
+      return "redirect:/userregister";
+    }
     }
 
 }
